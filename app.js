c8cc2c88b25d68caa3187f8c4e60ef5d6d09eac4
Fatima <fatimamughal291@gmail.com>
2020-07-26 17:20:43 +0500

    to do app complete

---
+++ app.js
@@ -0,0 +1,79 @@
+var list = document.getElementById("list");
+
+
+function addTodo(){
+    var todo_item = document.getElementById("todo-item");
+
+
+    // if (document.getElementById("todo-item").value == ""){
+    //     alert("please write your task")
+    //     return false;
+    // }
+
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+    li.setAttribute("class", "item_input")
+    li.setAttribute("class", "item")
+    li.appendChild(liText)
+
+    
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+    delBtn.setAttribute("class", "removeButton")
+    delBtn.setAttribute("onclick", "deleteItem(this)")
+    delBtn.appendChild(delText)
+
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("EDIT")
+    editBtn.setAttribute("class", "editButton")
+    editBtn.setAttribute("onclick", "editItem(this)")
+    editBtn.appendChild(editText)
+
+
+    // if (todo_item.value.length == 0)
+    // { 
+    //    alert("message");  	
+    //    return false; 
+    // }  	
+    // else return true; 
+
+
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+
+    // if( document.getElementById('todo-item').value === "" ){
+    //     alert('your field is empty');
+    // return false;
+    //   }
+    // else{
+    //     return true;
+    // }
+
+    list.appendChild(li)
+     todo_item.value = " "  
+     
+
+}
+
+
+function deleteItem(e){
+    e.parentNode.remove()
+}
+
+function editItem(e){
+    var val = e.parentNode.firstChild.nodeValue;
+    var editValue = prompt(  "Enter updated Value", e.parentNode.firstChild.nodeValue)
+    e.parentNode.firstChild.nodeValue = editValue;
+  
+}
+
+function deleteAll(){
+    list.innerHTML = " "
+}
+
+
+
+
+
+
+
